0f81dc3c9875f08ba2b3b1358a3d73e83b615343
adithya-s-k <adithyaskolavi@gmail.com>
2025-07-26 21:27:16 +0530

    tested math extractors : ready to merge - Adithya S K

--- omnidocs/tasks/math_expression_extraction/extractors/__init__.py
+++ omnidocs/tasks/math_expression_extraction/extractors/__init__.py
@@ -1,14 +1,10 @@
 # __init__.py in math_expression_extraction/extractors
 from .donut import DonutExtractor
-from .latex_ocr import LaTeXOCRExtractor
 from .nougat import NougatExtractor
-from .texify import TexifyExtractor
 from .unimernet import UniMERNetExtractor
 
 __all__ = [
     'DonutExtractor',
-    'LaTeXOCRExtractor',
     'NougatExtractor',
-    'TexifyExtractor',
     'UniMERNetExtractor'
 ]
